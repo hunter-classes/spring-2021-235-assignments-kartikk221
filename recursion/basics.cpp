213a998877ec6eb84288a3775c97591f9dd1088c
kartikk221 <kartikk221@yahoo.com>
2021-02-03 10:40:10 -0500

    Added Iteration Function

--- recursion/basics.cpp
+++ recursion/basics.cpp
@@ -4,7 +4,19 @@ int fib(int n){
     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
 }
 
+int fib_iter(int n){
+    int prev_number = 0;
+    int next_number = 1;
+    for(int i = 1; i <= n; i++){
+        int sum = prev_number + next_number;
+        prev_number = next_number;
+        next_number = sum;
+    }
+    return prev_number;
+}
+
 int main(){
-    std::cout << "Fibonacci Sequence Recursive @ (n = 9) -> " << fib(9) << std::endl;
+    std::cout << "Fibonacci Sequence Recursive @ (n = 9) -> " << fib(21) << std::endl;
+    std::cout << "Fibonacci Sequence Loop @ (n = 9) -> " << fib_iter(21) << std::endl;
     return 0;
 }
