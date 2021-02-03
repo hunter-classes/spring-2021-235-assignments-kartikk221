2edf31d83cf7a861a4f7e1fe507f7ce5e084e223
kartikk221 <kartikk221@yahoo.com>
2021-02-03 11:05:14 -0500

    Added David's Staircase

--- recursion/basics.cpp
+++ recursion/basics.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 int fib(int n){
     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
@@ -15,8 +16,27 @@ int fib_iter(int n){
     return prev_number;
 }
 
+/* David's Staircase Problem */
+std::map<int, int> steps_cache = {};
+int stepPerms(int n) {
+    switch(n){
+        case 1:
+            return 1;
+        case 2:
+            return 2;
+        case 3:
+            return 4;
+        default:
+            if(steps_cache.count(n) > 0)
+                return steps_cache[n];
+            steps_cache[n] = stepPerms(n - 1) + stepPerms(n - 2) + stepPerms(n - 3);
+            return steps_cache[n];
+    }
+}
+
 int main(){
-    std::cout << "Fibonacci Sequence Recursive @ (n = 9) -> " << fib(21) << std::endl;
-    std::cout << "Fibonacci Sequence Loop @ (n = 9) -> " << fib_iter(21) << std::endl;
+    std::cout << "Fibonacci Sequence Recursive @ (n = 9) -> " << fib(9) << std::endl;
+    std::cout << "Fibonacci Sequence Loop @ (n = 9) -> " << fib_iter(9) << std::endl;
+    std::cout << "David's Staircase Possibilities For 10 Stairs @ (n = 10) -> " << stepPerms(10) << std::endl;
     return 0;
 }
